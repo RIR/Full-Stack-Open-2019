787797b2b54c65a87582cccc392218e3d6f5fd54
Raine Rantanen <raine.ran7anen@gmail.com>
2019-08-24 20:29:12 +0300

    1.4 done

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -14,6 +14,7 @@ const Part = ({ name, count }) => (
     </p>
   </>
 );
+
 const Content = ({ parts }) => {
   return (
     <>
@@ -32,24 +33,26 @@ const Total = ({ total }) => (
 
 const App = () => {
   const course = 'Half Stack application development';
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  };
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  };
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  };
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ];
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={[part1, part2, part3]} />
-      <Total total={part1.exercises + part2.exercises + part3.exercises} />
+      <Content parts={parts} />
+      <Total total={parts.reduce((sum, part) => sum + part.exercises, 0)} />
     </div>
   );
 };
