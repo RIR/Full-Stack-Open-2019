1561787261cb927a611ea1a9c11a62c54777e693
Raine Rantanen <raine.ran7anen@gmail.com>
2020-02-27 19:03:23 +0200

    4.5 done

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -2,7 +2,19 @@ const dummy = blogs => 1;
 
 const totalLikes = blogs => blogs.reduce((sum, blog) => sum + blog.likes, 0);
 
+const favoriteBlog = blogs => {
+  const favoriteBlog = blogs.reduce((prev, current) => (prev.likes > current.likes ? prev : current), 0);
+  return favoriteBlog === 0
+    ? null
+    : {
+        title: favoriteBlog.title,
+        author: favoriteBlog.author,
+        likes: favoriteBlog.likes
+      };
+};
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 };
